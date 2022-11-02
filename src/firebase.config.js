0899452e977c8ad85a017460be1ad2b730bf5759
Enver Can Oude Hergelink <91742635+Hergelink@users.noreply.github.com>
2022-11-02 16:58:12 +0300

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfqsGVUd2uR6avpIQTWEYcv9WnTUEWM8k',
-  authDomain: 'studiohut-c69b8.firebaseapp.com',
-  projectId: 'studiohut-c69b8',
-  storageBucket: 'studiohut-c69b8.appspot.com',
-  messagingSenderId: '134315685813',
-  appId: '1:134315685813:web:8892069581a5c68aabcbfc',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
